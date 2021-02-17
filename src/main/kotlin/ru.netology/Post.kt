ac69a2f40ce958ac26c0d19a8851ac06aa4f2212
Mashnin Egor <y0g@mail.ru>
2021-02-18 05:02:45 +1000

    Readme added

--- src/main/kotlin/ru.netology/Post.kt
+++ src/main/kotlin/ru.netology/Post.kt
@@ -16,6 +16,7 @@ data class Post(
     val reposts: Reposts = Reposts(),
     val views: Views = Views(),
     val postType: String = "type",
+    val attachment: Array<Attachment>? = null,
     val signerId: Int = 0,
     val canPin: Boolean = false,
     val canDelete: Boolean = false,
@@ -24,4 +25,69 @@ data class Post(
     val markedAsAds: Boolean = false,
     val isFavourite: Boolean = false,
     val postponedId: Int = 0,
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Post
+
+        if (id != other.id) return false
+        if (ownerId != other.ownerId) return false
+        if (fromId != other.fromId) return false
+        if (createdBy != other.createdBy) return false
+        if (date != other.date) return false
+        if (text != other.text) return false
+        if (replyOwnerId != other.replyOwnerId) return false
+        if (replyPostId != other.replyPostId) return false
+        if (friendsOnly != other.friendsOnly) return false
+        if (comments != other.comments) return false
+        if (copyright != other.copyright) return false
+        if (likes != other.likes) return false
+        if (reposts != other.reposts) return false
+        if (views != other.views) return false
+        if (postType != other.postType) return false
+        if (attachment != null) {
+            if (other.attachment == null) return false
+            if (!attachment.contentEquals(other.attachment)) return false
+        } else if (other.attachment != null) return false
+        if (signerId != other.signerId) return false
+        if (canPin != other.canPin) return false
+        if (canDelete != other.canDelete) return false
+        if (canEdit != other.canEdit) return false
+        if (isPinned != other.isPinned) return false
+        if (markedAsAds != other.markedAsAds) return false
+        if (isFavourite != other.isFavourite) return false
+        if (postponedId != other.postponedId) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id
+        result = 31 * result + ownerId
+        result = 31 * result + fromId
+        result = 31 * result + createdBy
+        result = 31 * result + date
+        result = 31 * result + text.hashCode()
+        result = 31 * result + replyOwnerId
+        result = 31 * result + replyPostId
+        result = 31 * result + friendsOnly.hashCode()
+        result = 31 * result + comments.hashCode()
+        result = 31 * result + copyright.hashCode()
+        result = 31 * result + likes.hashCode()
+        result = 31 * result + reposts.hashCode()
+        result = 31 * result + views.hashCode()
+        result = 31 * result + postType.hashCode()
+        result = 31 * result + (attachment?.contentHashCode() ?: 0)
+        result = 31 * result + signerId
+        result = 31 * result + canPin.hashCode()
+        result = 31 * result + canDelete.hashCode()
+        result = 31 * result + canEdit.hashCode()
+        result = 31 * result + isPinned.hashCode()
+        result = 31 * result + markedAsAds.hashCode()
+        result = 31 * result + isFavourite.hashCode()
+        result = 31 * result + postponedId
+        return result
+    }
+}
